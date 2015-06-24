0899288c151f845c1550645e2b2ded3a570b42b2
Epicurean306 <benjaminforbesgriffith@gmail.com>
2015-06-24 16:27:16 -0400

    more mockup HTML and JS for practice

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,17 @@
-console.log('\'Allo \'Allo!');
+(function(window){
+  var app = angular.module('tiy-gradebook', [ ]);
+
+  app.controller('MainController', function(){
+    this.view = null;
+    this.page = function(name){
+      this.view = 'view/404.html';
+      if ( name == 'milestones' ){
+        this.view = 'views/milestones.html';
+      }
+      if ( name == 'repositories' ){
+        this.view = 'views/repositories.html';
+      }
+    };
+  }); // END controller(MainController)
+
+})(window);
